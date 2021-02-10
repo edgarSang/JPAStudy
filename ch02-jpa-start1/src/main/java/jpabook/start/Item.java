762dea13c83ef19082283c83df96ef380f5d6fd2
SangHa.Han <sangha.han@linecorp.com>
2021-02-10 23:47:33 +0900

    Before Jpa example

---
+++ ch02-jpa-start1/src/main/java/jpabook/start/Item.java
@@ -0,0 +1,21 @@
+package jpabook.start;
+
+abstract public class Item {
+    Long id;
+    String name;
+    int price;
+}
+
+class Album extends Item {
+    String artist;
+}
+
+class Movie extends Item {
+    String director;
+    String actor;
+}
+
+class Book extends Item {
+    String author;
+    String isbn;
+}
---
+++ ch02-jpa-start1/src/main/java/jpabook/start/TmpMember.java
@@ -0,0 +1,14 @@
+package jpabook.start;
+
+public class TmpMember {
+    private String memberId;
+    private String name;
+    private String tel;
+    private Team team;
+    private Long teamId;
+
+}
+class Team {
+    private Long id;
+    private String TeamName;
+}
